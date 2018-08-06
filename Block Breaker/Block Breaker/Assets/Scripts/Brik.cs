bd3315fd083bf19ca52149cdc93bc43749bb95ff
kanghy22 <nasmurf1hk@gmail.com>
2018-08-06 16:09:35 -0400

    working on flow conditions

---
+++ Block Breaker/Block Breaker/Assets/Scripts/Brik.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Brik : MonoBehaviour {
+
+	public int maxHits;
+
+	private int timesHits;
+
+	private LevelManager levelManager;
+
+	// Use this for initialization
+	void Start () {
+		timesHits = 0;
+		levelManager = GameObject.FindObjectOfType<LevelManager> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D collision) {
+		timesHits++;
+
+		SimulateWin ();
+	}
+
+	// TODO: Remove this method once we can actually win
+	void SimulateWin() {
+		levelManager.LoadNextLevel ();
+	}
+}
---
+++ Block Breaker/Block Breaker/Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Collider script to catch the Game Over trigger
+/// </summary>
+public class LoseCollider : MonoBehaviour {
+
+	private LevelManager levelManager;
+
+	void Start () {
+		levelManager = GameObject.FindObjectOfType<LevelManager> ();
+	}
+
+	void OnTriggerEnter2D(Collider2D trigger) {
+		levelManager.LoadLevel ("Lose Screen");
+	}
+}
